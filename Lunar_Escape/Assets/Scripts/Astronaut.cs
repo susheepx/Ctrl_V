58b99b3fca82cd2a6f61922659ad6f2773936a4b
susheepx <erica1234lu@gmail.com>
2022-05-13 07:27:02 -0700

    Character Movement (Done)
    - improved character movement (removed jittering when colliding with object)
    - camera movement smoothed out
    - Tested walls with filler black space outside room
    - smoothed out walking anim (added middle frame; standing still)

--- Lunar_Escape/Assets/Scripts/Astronaut.cs
+++ Lunar_Escape/Assets/Scripts/Astronaut.cs
@@ -19,16 +19,20 @@ public class Astronaut : MonoBehaviour
     private void Update() {
         PlayerMoveKeyboard();
         AnimatePlayer();
-        rb.velocity = new Vector2(movementX, movementY);
     }
     void PlayerMoveKeyboard() {
         movementX = Input.GetAxisRaw("Horizontal");
         movementY = Input.GetAxisRaw("Vertical");
 
-        if(movementX >= movementY || movementY == 0)
-            transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
-        if(movementY > movementX || movementX == 0)
-            transform.position += new Vector3(0f, movementY, 0f) * Time.deltaTime * moveForce;
+        if(movementY != 0 && movementX == 0)
+            rb.velocity = new Vector2(0f, movementY) * moveForce;
+        else if(movementX != 0)
+        {
+            rb.velocity = new Vector2(movementX, 0f) * moveForce;
+        }
+        else
+            rb.velocity = new Vector2(0f, 0f);
+    
     }
 
     void AnimatePlayer() {
@@ -47,7 +51,6 @@ public class Astronaut : MonoBehaviour
         }
         if(movementX == 0 && movementY == 0)
             {
-                Debug.Log("stop animation");
                 anim.SetBool("Left", false);
                 anim.SetBool("Right", false);
 
