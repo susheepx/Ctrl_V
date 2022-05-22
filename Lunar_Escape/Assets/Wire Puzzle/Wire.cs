6237beab7e26fdaf5ac84ba8b83f81b5b5d4af17
susheepx <erica1234lu@gmail.com>
2022-05-22 08:07:59 -0700

    1.24.522
    - continued work on wires, trying to fix not being able to count int issue

--- Lunar_Escape/Assets/Wire Puzzle/Wire.cs
+++ Lunar_Escape/Assets/Wire Puzzle/Wire.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class Wire : MonoBehaviour
 {
+    public int numOfConnected;
+    private bool isWireConnected = false;
+    
     Vector3 startPoint;
     Vector3 startPosition;
+    public GameObject lightOn;
     public SpriteRenderer wireEnd;
     private void Start() {
         startPoint = transform.parent.position;
         startPosition = transform.position;
     }
     private void OnMouseDrag() {
+        isWireConnected = false;
+        lightOn.SetActive(false);
         //mouse position to world point
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         newPosition.z = 0;
@@ -21,10 +27,17 @@ public class Wire : MonoBehaviour
         foreach (Collider2D collider in colliders)
         {
             //make sure it is not my own collider
-            if (collider.gameObject != gameObject) {
+            if (collider.gameObject != gameObject && collider.gameObject.transform.parent != gameObject.transform.parent) {
 
                 //update wire position to connect to other wire
                 UpdateWire(collider.transform.position);
+
+                // check if the wire connection is correct
+                //if (transform.parent.name.Equals(collider.transform.parent.name)) {
+                    //finish step
+                collider.GetComponent<Wire>()?.Done();
+                Done();
+                //}
                 return;
             }
         }
@@ -32,25 +45,38 @@ public class Wire : MonoBehaviour
         //update wire
         UpdateWire(newPosition);
         
-        //update direction
-        Vector3 direction = newPosition - startPoint;
-        transform.right = direction * transform.lossyScale.x;
     }
 
+    private void Done() {
+        //turn on light
+        lightOn.SetActive(true);
+        isWireConnected = true;
+        
+    }
+    
     private void OnMouseUp() {
+        
         // reset wire position
-        UpdateWire(startPosition);
-        Vector3 newPosition = startPosition;
-        //update direction
-        Vector3 direction = newPosition - startPoint;
-        transform.right = direction * -transform.lossyScale.x;
+        if (! isWireConnected) {
+            UpdateWire(startPosition);
+            CheckWires();
+        }
+    
+    void CheckWires() {
+        if (isWireConnected) {
+            numOfConnected = numOfConnected + 1;
+        }
+    }
+    
     }
 
     void UpdateWire(Vector3 newPosition) {
         // update position
         transform.position = newPosition;
                
-
+        
+        Vector3 direction = newPosition - startPoint;
+        transform.right = direction * transform.lossyScale.x;
         
 
         //update scale
